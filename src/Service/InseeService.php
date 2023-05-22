aabc6fb1b272a0c6c35178a40496ee1f3cbbde8f
Djyp <djyp@plus.yt>
2023-05-22 08:46:45 +0200

    Ajout de la route API vers l'INSEE pour la page d'accueil

---
+++ src/Service/InseeService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Service;
+
+class InseeService
+{
+    public function getInformationFomSiret(string $siret)
+    {
+        $token = $this->getToken();
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.insee.fr/entreprises/sirene/V3/siret/' . $siret,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+              'Accept: application/json',
+              'Authorization: Bearer ' . $token,
+            ),
+          ));
+
+        $responseJson = curl_exec($curl);
+        $response = json_decode($responseJson);
+        curl_close($curl);
+        $informations = [];
+        $informations['CodePostal'] = $response->etablissement->adresseEtablissement->codePostalEtablissement;
+
+        return $informations;
+    }
+
+    public function getToken()
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.insee.fr/token',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => 'grant_type=client_credentials',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Basic ' . $_ENV['INSEE_API_AUTH'],
+            'Content-Type: application/x-www-form-urlencoded'
+        ),
+        ));
+
+        $response = json_decode(curl_exec($curl));
+        curl_close($curl);
+        return $response->access_token;
+    }
+}
