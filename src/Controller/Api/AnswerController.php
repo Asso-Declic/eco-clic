c04b1e20bf2ce6e4358218294d92ced31822446f
Djyp <djyp@plus.yt>
2023-06-16 17:49:30 +0200

    Précisions sur les déclaration de routes API

--- src/Controller/Api/AnswerController.php
+++ src/Controller/Api/AnswerController.php
@@ -12,20 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api/answer', name: 'api_answer_')]
 class AnswerController extends AbstractController
 {
-    #[Route('/ponderation-max', name: 'ponderation_max')]
+    #[Route('/ponderation-max', name: 'ponderation_max', methods: ['GET'])]
     public function ponderationMax(AnswerRepository $answerRepository): JsonResponse
     {
         return $this->json(['data' => $answerRepository->findPonderationMax()]);
     }
 
-    #[Route('/by-question/{id}', name: 'by_question')]
+    #[Route('/by-question/{id}', name: 'by_question', methods: ['GET'])]
     public function byQuestion(AnswerRepository $answerRepository, Question $question): JsonResponse
     {
         $questions = $answerRepository->findBy(['question' => $question]);
         return $this->json(['data' => $questions], 200, [], ['groups' => 'answer']);
     }
 
-    #[Route('/{id}', name: 'read', requirements: ['id' => '^[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}$'])]
+    #[Route('/{id}', name: 'read', methods: ['GET'], requirements: ['id' => '^[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}$'])]
     public function read(Answer $answer)
     {
         return $this->json(['data' => $answer]);
