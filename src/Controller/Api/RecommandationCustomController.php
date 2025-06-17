fee0b04787965b770a8ffd13d51f2fc2cdc98f02
lucasGoudry <113920307+lucasGoudry@users.noreply.github.com>
2025-06-17 11:22:39 +0200

    reco perso

--- src/Controller/Api/RecommandationCustomController.php
+++ src/Controller/Api/RecommandationCustomController.php
@@ -3,16 +3,20 @@
 namespace App\Controller\Api;
 
 use App\Entity\Category;
-use App\Entity\Collectivite;
 use App\Entity\Question;
+use App\Entity\Collectivite;
 use App\Entity\Recommandation;
+use App\Entity\RecommandationLevel;
+use App\Entity\RecommandationPerso;
 use App\Entity\RecommandationCustom;
-use App\Repository\RecommandationCustomRepository;
+use App\Entity\RecommandationStatus;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RecommandationPersoRepository;
+use App\Repository\RecommandationCustomRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/api/recommendation-customs', name: 'api_recommandation_custom_')]
 class RecommandationCustomController extends AbstractController
@@ -34,7 +38,7 @@ class RecommandationCustomController extends AbstractController
         
         $recommandationCustoms = $recommandationCustomRepository->delete($data['collectivite'], $data['question']);
         
-        if (isset($data['recommandations'])) {
+        if ($data['recommandations'] != '') {
             foreach($data['recommandations'] as $recommandation) {
                 
                 $recommandation = $em->getRepository(Recommandation::class)->find($recommandation);
@@ -46,6 +50,35 @@ class RecommandationCustomController extends AbstractController
 
                 $em->persist($recommandationCustom);
             }
+            $em->flush();
+        }
+
+        return $this->json(['message' => 'ok'], 200);
+    }
+
+    #[Route('/perso', name: 'addPerso', methods: ['POST'])]
+    public function addPerso(EntityManagerInterface $em, Request $request, RecommandationPersoRepository $recommandationPersoRepository): JsonResponse
+    {
+        $data = $request->request->all();
+
+        $collectivite = $em->getRepository(Collectivite::class)->find($data['collectivite']);
+        $question = $em->getRepository(Question::class)->find($data['question']);
+        $level = $em->getRepository(RecommandationLevel::class)->find("1");
+        $status = $em->getRepository(RecommandationStatus::class)->find("4");
+        
+        $recommandationPerso = $recommandationPersoRepository->delete($data['collectivite'], $data['question']);
+        
+        if ($data['recommandation'] != '') {
+            
+            $recommandationPerso = new RecommandationPerso();
+            $recommandationPerso->setTitle($question->getQuestion());
+            $recommandationPerso->setBody($data['recommandation']);
+            $recommandationPerso->setQuestion($question);
+            $recommandationPerso->setCollectivite($collectivite);
+            $recommandationPerso->setLevel($level);
+            $recommandationPerso->setStatus($status);
+
+            $em->persist($recommandationPerso);
             $em->flush();
         }
 
